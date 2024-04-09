3502a33849795df588cf3b838ee5de812b63859e
Valerij Komarov <komarov_v@garpix.com>
2024-04-09 15:57:37 +0300

    Edit and pageinfo

---
+++ diplom/diplom/Views/EditTransactionPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using diplom.ComponentsInterface;
+using diplom.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace diplom.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditTransactionPage : ContentPage
+    {
+        public EditTransactionPage()
+        {
+            InitializeComponent();
+
+            EntryInterface entryInterface = new EntryInterface();
+            entryInterface.OnlyNumbers(entrySum);
+        }
+
+        protected override async void OnAppearing()
+        {
+            List<Category> categories = await App.Diplomdatabase.GetCategoriesAsync();
+            pickerCategory.ItemsSource = categories;
+
+            if (BindingContext is Transaction transaction)
+            {
+                int type = 0;
+                if (transaction.Type != 1)
+                    type = 1;
+
+                pickerType.SelectedIndex = type;
+                entrySum.Text = transaction.Sum.ToString();
+                DatePicker.Date = transaction.Date;
+                pickerCategory.SelectedIndex = transaction.CategoryId - 1;
+            }
+            base.OnAppearing();
+        }
+
+        private async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            if (BindingContext is Transaction transaction)
+            {
+                int type = 1;
+                if (pickerType.SelectedIndex != 0)
+                    type = -1;
+                // Обновление данных транзакции на основе введенных значений
+                transaction.Type = type;
+                transaction.Sum = Convert.ToDecimal(entrySum.Text);
+                transaction.Date = DatePicker.Date;
+                transaction.CategoryId = pickerCategory.SelectedIndex + 1;
+
+                await App.Diplomdatabase.SaveTransactionAsync(transaction);
+
+                await Navigation.PopAsync();
+            }
+        }
+    }
+}
